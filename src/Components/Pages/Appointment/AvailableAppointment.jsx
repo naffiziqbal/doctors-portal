a17dce049b0cc61942f0689b4bfeeb76a117be19
naffiziqbal <f.nafiziqbal@gmail.com>
2022-11-17 22:14:03 +0600

    using UseQuery Hook

--- src/Components/Pages/Appointment/AvailableAppointment.jsx
+++ src/Components/Pages/Appointment/AvailableAppointment.jsx
@@ -1,16 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
 import { format } from "date-fns";
 import React, { useEffect, useState } from "react";
 import BookingInfo from "../../BookingInfo/BookingInfo";
 import AppoionementOptions from "./AppoionementOptions";
 
 const AvailableAppointment = ({ selectedDay, letSelectedDay }) => {
-  const [appointmentOpt, setAppointmentOpt] = useState([]);
-  const [ treatment , setTreatment] = useState({})
-  useEffect(() => {
-    fetch(`http://localhost:5000/appointmentOptions`)
-      .then((res) => res.json())
-      .then((data) => setAppointmentOpt(data));
-  }, []);
+  // const [appointmentOpt, setAppointmentOpt] = useState([]);
+  const [treatment, setTreatment] = useState({});
+
+  const {data:appointmentOpt = [], isLoading} = useQuery({
+    queryKey: ["appointmentOpt"],
+    queryFn: () => fetch(`http://localhost:5000/appointmentOptions`)
+    .then(res => res.json())
+  });
+  // useEffect(() => {
+  //     .then((res) => res.json())
+  //     .then((data) => setAppointmentOpt(data));
+  // }, []);
   return (
     <>
       <div>
@@ -20,16 +26,15 @@ const AvailableAppointment = ({ selectedDay, letSelectedDay }) => {
         </p>
       </div>
       <div className="appointmentOption grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5">
-        {appointmentOpt.map((option) => (
-          <AppoionementOptions key={appointmentOpt._id} option={option} 
-          setTreatment={setTreatment}
+        {appointmentOpt?.map((option) => (
+          <AppoionementOptions
+            key={appointmentOpt?._id}
+            option={option}
+            setTreatment={setTreatment}
           />
         ))}
       </div>
-      <BookingInfo
-      treatment={treatment}
-      selectedDay={selectedDay}
-      />
+      <BookingInfo treatment={treatment} selectedDay={selectedDay} />
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'react-day-picker/dist/style.css';
-import UserContext, { AuthProvider } from './USerContext/UserContext';
-
+import UserContext from './USerContext/UserContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <UserContext>
       <App />
     </UserContext>
-  </React.StrictMode>
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
